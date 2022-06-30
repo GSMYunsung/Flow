3584fbac20316da76b47fd17ad37d50cd916fac8
choiyunsung <12345ggh81@gmail.com>
2022-06-30 21:08:37 +0900

    [DELETE] List Saver

---
+++ app/src/main/java/com/yunsung/flow/flow_blocking_nonblocking/Helper.kt
@@ -0,0 +1,8 @@
+package com.yunsung.flow
+
+import kotlinx.coroutines.flow.flowOf
+
+val userList = listOf("User1","User2","User3","User4","User5")
+
+// flow를 변수로 선언하는 방법
+val flowUserList = flowOf("User6","User7","User8","User9","User10")
