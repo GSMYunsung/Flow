5db3add28414cd89d3efd513a7ddaa42e1f61d54
choiyunsung <12345ggh81@gmail.com>
2022-07-25 20:57:35 +0900

    [CREATE] transform 형식 변환 연산자

---
+++ app/src/main/java/com/yunsung/flow/flow_transform/TransformActivity.kt
@@ -0,0 +1,33 @@
+package com.yunsung.flow.flow_transform
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
+import com.yunsung.flow.R
+import com.yunsung.flow.ageList
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.transform
+import kotlinx.coroutines.launch
+
+class TransformActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_common)
+
+        // 중간연산자 (flow) 는 요소마다 1 개의 변환 밖에 하지못하지만
+        // 변환연산자 (transform) 은 매 주기마다 여러개의 변환이 가능합니다.
+
+       lifecycleScope.launch(Dispatchers.IO) {
+            ageList.asFlow()
+                .transform { age ->
+                   emit("Age : $age . The Value is Getting transformed")
+                   emit("Age : $age")
+                }
+                .collect{
+                    value -> Log.d("TAG" , value)
+                }
+        }
+    }
+}
