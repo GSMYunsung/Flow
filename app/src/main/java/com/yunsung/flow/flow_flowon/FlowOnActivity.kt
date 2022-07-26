cc7bc8c6cd8dbeb3e476e72a20c7f5c2aeec8548
choiyunsung <12345ggh81@gmail.com>
2022-07-26 23:32:25 +0900

    [CREATE] flowOn 으로 collection 을 호출한 caller 의 스레드에서 처리하지 않도록 처리

---
+++ app/src/main/java/com/yunsung/flow/flow_flowon/FlowOnActivity.kt
@@ -0,0 +1,39 @@
+package com.yunsung.flow.flow_flowon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
+import com.yunsung.flow.R
+import com.yunsung.flow.ageList
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+
+class FlowOnActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_common)
+
+         lifecycleScope.launch {
+                ageFlow().onCompletion {  Log.d("TAG","${Thread.currentThread().name}Flow has completed") }
+                    .collect{ value ->
+                    Log.d("TAG","${Thread.currentThread().name} $value")
+            }
+
+        }
+
+    }
+
+    private fun ageFlow(): Flow<Int> = ageList.asFlow()
+        .transform { age ->
+            Log.d("TAG", "${Thread.currentThread().name}")
+
+            if (age == 30) {
+                emit(age + 1)
+            } else {
+                emit(age)
+            }
+
+        }.flowOn(Dispatchers.IO)
+}
