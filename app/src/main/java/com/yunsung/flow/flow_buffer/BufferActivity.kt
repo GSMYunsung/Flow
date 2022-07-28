36c1e4f90f1710730cab19a294bb6b270056028c
choiyunsung <12345ggh81@gmail.com>
2022-07-28 22:14:49 +0900

    [CREATE] Buffer로 효율성 증가

---
+++ app/src/main/java/com/yunsung/flow/flow_buffer/BufferActivity.kt
@@ -0,0 +1,38 @@
+package com.yunsung.flow.flow_buffer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
+import com.yunsung.flow.R
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.conflate
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+
+class BufferActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_common)
+
+        lifecycleScope.launch { numFlow()
+            //.buffer()
+            //.conflate()
+            .collect{ it->
+                Log.d("TAG" , "Log2 : ${Thread.currentThread().name} : $it")
+
+            } }
+
+    }
+}
+
+
+private fun numFlow() : Flow<Int> = flow{
+    for( i in 1..10 ){
+        Log.d("TAG" , "Log1 : ${Thread.currentThread().name}")
+        emit(i)
+    }
+
+
+}
