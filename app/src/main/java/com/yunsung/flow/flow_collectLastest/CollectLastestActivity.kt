ea6f71ff2add5020a0c9d9d4b130571cbee98c96
choiyunsung <12345ggh81@gmail.com>
2022-07-28 22:15:30 +0900

    [CREATE] CollectLastest 를 통해 실시간 값 변경 확인 및 emit 과정 즉시종료

---
+++ app/src/main/java/com/yunsung/flow/flow_collectLastest/CollectLastestActivity.kt
@@ -0,0 +1,39 @@
+package com.yunsung.flow.flow_collectLastest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
+import com.yunsung.flow.R
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+
+class CollectLastestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_common)
+
+        lifecycleScope.launch { numFlow()
+            .collectLatest {
+
+                delay(200)
+                Log.d("TAG", "Log2 : ${Thread.currentThread().name}")
+            }
+        }
+
+    }
+}
+
+private fun numFlow() : Flow<Int> = flow{
+    for( i in 1..10 ){
+        delay(100)
+        Log.d("TAG" , "Log1 : ${Thread.currentThread().name}")
+        emit(i)
+    }
+
+
+}
