a2d37170513f5530c64949c33843a9991f799fa6
choiyunsung <12345ggh81@gmail.com>
2022-08-05 22:58:58 +0900

    [UPDATE] 기본 update

--- app/src/main/java/com/yunsung/flow/flow_blocking_nonblocking/Helper.kt
+++ app/src/main/java/com/yunsung/flow/flow_blocking_nonblocking/Helper.kt
@@ -4,7 +4,7 @@ import kotlinx.coroutines.flow.flowOf
 
 val userList = listOf("User1","User2","User3","User4","User5")
 
-val ageList = listOf(1,2,3,4,5)
+val ageList = listOf(10,20,30,40,50)
 
 // flow를 변수로 선언하는 방법
 val flowUserList = flowOf("User6","User7","User8","User9","User10")
